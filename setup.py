9acdad23d85a29ff73d263748741f64a8cb1d9c5
wgp <wgp2045@163.com>
2026-06-14 13:53:29 +0800

    feat: 更新版本号至1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hutool-python",
-    version="1.0.0",
+    version="1.0.1",
     description="Python port of Java Hutool utility library",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
